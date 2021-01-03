a7d7a47ad918db5e2b489e6171ed1d2e57a13b56
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-01-03 19:16:16 +0200

    Update BTCVoucherGen.py

--- BTCVoucherGen.py
+++ BTCVoucherGen.py
@@ -1,13 +1,1 @@
-import requests
-import random
-chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-number = input('[❌] Введите количество ссылок для генерации:'+ "\n")
-length = input('[❌] Введите длинну ваучера (по стандарту '+ "\n")
-btc_link = 'https://t.me/BTC_CHANGE_BOT?start=b_'
-number = int(number)
-length = int(length)
-for n in range(number):
-   password =''
-   for i in range(length):
-       password += random.choice(chars)
-   print(btc_link + password)
+print("[⚠️] Произошла ошибка")
