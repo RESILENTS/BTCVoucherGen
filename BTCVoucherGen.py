47f215abc043dedba55409f8bfb1c4f6d3ae7b69
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-01-04 01:53:00 +0200

    Update BTCVoucherGen.py

--- BTCVoucherGen.py
+++ BTCVoucherGen.py
@@ -3,9 +3,11 @@ import random
 chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 number = int(input('количество паролей?'+ "\n"))
 
+btc_link = 'link'
+
 length = int(input('длина пароля?'+ "\n"))
 for n in range(number):
     password =''
     for i in range(length):
         password += random.choice(chars)
-    print(password)
+    print('ghh', password)
