982eda5c677c723d73003d21c329a3a660799e5c
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-01-03 19:17:13 +0200

    Update BTCVoucherGen.py

--- BTCVoucherGen.py
+++ BTCVoucherGen.py
@@ -1,1 +1,12 @@
-print("[⚠️] Произошла ошибка")
+import requests
+from colorama import Fore, Back, Style
+
+banner = Fore.YELLOW + Style.BRIGHT + """
+  \  __\    /\__  _\    /\ \  __/\ 
+\ \ \/\ \   \/_/\ \/    \ \ \/\ \ \ \  \/_/\ 
+\ \ \ \ \    \ \ \     \ \ \ \ \ \ \    \ \ \
+\ \ \ \ \     \_\ \__   \ \ \_/ \_\ \    \_\ \
+  by @li0ard [ovnl.in] |   t.me/termuxmam / PIRATIKA
+""" + Style.RESET_ALL
+
+print(banner)
