f53059fc14298ad7da61bf6a456e2aaad3f864a3
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-01-04 01:57:51 +0200

    Update BTCVoucherGen.py

--- BTCVoucherGen.py
+++ BTCVoucherGen.py
@@ -1,4 +1,12 @@
 import random
+import requestsfrom colorama import Fore, Back, Style 
+
+banner = Fore.YELLOW + Style.BRIGHT + """ \ __\ /\__ _\ /\ \ __/\ \ \ \/\ \ \/_/\ \/ \ \ \/\ \ \ \ \/_/\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \\ \ \ \ \ \_\ \__ \ \ \_/ \_\ \ \_\ \ 
+by @li0ard [ovnl.in] | t.me/termuxmam / PIRATIKA
+
+""" + Style.RESET_ALL 
+print(banner)
+
 
 chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 number = int(input('количество паролей?'+ "\n"))
