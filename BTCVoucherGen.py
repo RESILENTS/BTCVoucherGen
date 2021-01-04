9c0579041884ab46f294d4c4c40246152f621729
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-01-04 02:01:59 +0200

    Update BTCVoucherGen.py

--- BTCVoucherGen.py
+++ BTCVoucherGen.py
@@ -1,14 +1,9 @@
 import random
-import requestsfrom colorama import Fore, Back, Style 
-
-banner = Fore.YELLOW + Style.BRIGHT + """ \ __\ /\__ _\ /\ \ __/\ \ \ \/\ \ \/_/\ \/ \ \ \/\ \ \ \ \/_/\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \\ \ \ \ \ \_\ \__ \ \ \_/ \_\ \ \_\ \ 
-by @li0ard [ovnl.in] | t.me/termuxmam / PIRATIKA
-
-""" + Style.RESET_ALL 
-print(banner)
 
 
-chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
+
+chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 number = int(input('количество паролей?'+ "\n"))
 
 btc_link = 'link'
@@ -18,4 +13,4 @@ for n in range(number):
     password =''
     for i in range(length):
         password += random.choice(chars)
-    print('ghh', password)
+    print(btc_link, password)
