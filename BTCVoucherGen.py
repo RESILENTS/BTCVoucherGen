a316147a05cc6084fa27d3559c0fd6417947369f
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-01-04 03:34:03 +0200

    Update BTCVoucherGen.py

--- BTCVoucherGen.py
+++ BTCVoucherGen.py
@@ -1,15 +1,20 @@
 import random
 
-print('➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖');
+print('➖➖➖➖➖➖➖[ BTCVoucherGen ]➖➖➖➖➖➖➖')
+print(' ')
+print('Генератор ваучеров для @BTC_CHANGE_BOT.')
+print('Скрипт написан для канала ❌ ТЕНЕВОЙ БИЗНЕС ❌')
+print('Вступить в канал: t.me/shadowbiznes | Наш чат: t.me/shadowbchat')
+print(' ')
+print('➖➖➖➖➖➖➖[ BTCVoucherGen ]➖➖➖➖➖➖➖')
 chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-number = int(input('количество паролей?'+ "\n"))
-
-btc_link = 'link'
-
-length = int(input('длина пароля?'+ "\n"))
+number = input('[❌] Введите желаемое количество ссылок для генерации.'+ "\n")
+length = input('[❌] Введите число символов в ваучере (по стандарту 32).'+ "\n")
+number = int(number)
+length = int(length)
+print('[✅] Успешно сгенерировали ', length, ' ссылок.')
 for n in range(number):
     password =''
     for i in range(length):
         password += random.choice(chars)
-
-print("btc_link", password)
+    print('https://t.me/BTC_CHANGE_BOT?start=b_', password)
