71717799e5165da5c081e6450cf04a260f5ee620
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-01-10 01:15:47 +0200

    Update and rename BTCVoucherGen.py to BTCVoucherGen1.py

---
+++ BTCVoucherGen1.py
@@ -0,0 +1,27 @@
+import random
+
+print(' ')
+print('➖➖➖➖➖➖➖[ BTCCheckGen 1.0 ]➖➖➖➖➖➖➖')
+print(' ')
+print('Генератор чеков для @BTC_CHANGE_BOT')
+print('Скрипт написан для канала ТЕНЕВОЙ БИЗНЕС')
+print('Автор: t.me/resilents | Наши проекты: t.me/shadowbiznes')
+print(' ')
+print('➖➖➖➖➖➖➖[ BTCCheckGen 1.0 ]➖➖➖➖➖➖➖')
+print(' ')
+chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+number = input('◾ Укажите желаемое количество ссылок для генерации:'+ "\n")
+print(' ')
+length = input('◾ Укажите длинну чека (советуем ввести 32):'+ "\n")
+print(' ')
+number = int(number)
+length = int(length)
+print(' ')
+print('☑ Скрипт успешно сгенерировал ', number, ' чеков.')
+print('⚠️ ВНИМАНИЕ: Скрипт генерирует рандомные чеки, проверять на валидность придется вручную.')
+print(' ')
+for n in range(number):
+    password =''
+    for i in range(length):
+        password += random.choice(chars)
+    print('https://t.me/BTC_CHANGE_BOT?start=b_',password)
