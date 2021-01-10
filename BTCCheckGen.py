c0ae640b6409cc3934636d3c37a769dfc32ac84a
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-01-10 08:09:35 +0200

    Update BTCCheckGen.py

--- BTCCheckGen.py
+++ BTCCheckGen.py
@@ -15,19 +15,17 @@ print('1️⃣ BTC_EXCHANGE_BOT')
 print('2️⃣ CHATEX_BOT')
 print()
 inp = input("[BTCCheckBot] Ваш выбор: ")
+
+if inp == '1':
 chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 number = input('[BTCCheckGen] ➕ Укажите желаемое количество ссылок для генерации:'+ "\n")
 print(' ')
-length = input('[BTCCheckGen] ➕ Укажите длинну чека (советуем ввести 32):'+ "\n")
-print(' ')
 number = int(number)
-length = int(length)
+length = '32'
 print(' ')
 print('➖➖➖➖➖➖➖[ BTCCheckGen 1.1 ]➖➖➖➖➖➖➖')
 print(' ')
 print('[BTCCheckGen] ✅ Работа скрипта успешно завершена: ', number, ' чеков сгенерировано.')
-print('[BTCCheckGen] ⚠️ ВНИМАНИЕ: Скрипт генерирует рандомные чеки, проверять на валидность придется вручную.')
-print(' ')
 print('➖➖➖➖➖➖➖[ BTCCheckGen 1.1 ]➖➖➖➖➖➖➖')
 print(' ')
 for n in range(number):
@@ -35,8 +33,25 @@ for n in range(number):
     for i in range(length):
         password += random.choice(chars)
     print('https://t.me/BTC_CHANGE_BOT?start=b_',password)
+
+
+elif inp == '2':
+chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+number = input('[BTCCheckGen] ➕ Укажите желаемое количество ссылок для генерации:'+ "\n")
 print(' ')
-print('[BTCCheckGen] ℹ Все новости и обновления скрипта в нашем телеграм канале, подпишись и не пропускай новые плюшки.')
+number = int(number)
+length = '32'
 print(' ')
 print('➖➖➖➖➖➖➖[ BTCCheckGen 1.1 ]➖➖➖➖➖➖➖')
 print(' ')
+print('[BTCCheckGen] ✅ Работа скрипта успешно завершена: ', number, ' чеков сгенерировано.')
+print('➖➖➖➖➖➖➖[ BTCCheckGen 1.1 ]➖➖➖➖➖➖➖')
+print(' ')
+for n in range(number):
+    password =''
+    for i in range(length):
+        password += random.choice(chars)
+    print('https://t.me/BTC_CHANGE_BOT?start=b_',password)
+else:
+    print("[ProxyGo] ⛔ Произошла ошибка! Повторите попытку.")
+toexit = input("[ProxyGo] ⚠️ Нажмите любую клавишу для завершения.")
