9f659aac413228c3b5bdd243d22918b3bd33f89f
RESILENTS <60385275+RESILENTS@users.noreply.github.com>
2021-01-10 08:18:32 +0200

    Update BTCCheckGen.py

--- BTCCheckGen.py
+++ BTCCheckGen.py
@@ -18,42 +18,64 @@ print('2️⃣ CHATEX_BOT')
 print()
 inp = input("[BTCCheckBot] Ваш выбор: ")
 Print()
+
 if inp == '1':
-print()
+
+chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 number = input('[BTCCheckGen] ➕ Укажите желаемое количество ссылок для генерации:'+ "\n")
 print(' ')
+length = input('[BTCCheckGen] ➕ Укажите длинну чека (советуем ввести 32):'+ "\n")
+print(' ')
 number = int(number)
-length = '32'
+length = int(length)
 print(' ')
 print('➖➖➖➖➖➖➖[ BTCCheckGen 1.1 ]➖➖➖➖➖➖➖')
 print(' ')
 print('[BTCCheckGen] ✅ Работа скрипта успешно завершена: ', number, ' чеков сгенерировано.')
+print('[BTCCheckGen] ⚠️ ВНИМАНИЕ: Скрипт генерирует рандомные чеки, проверять на валидность придется вручную.')
+print(' ')
 print('➖➖➖➖➖➖➖[ BTCCheckGen 1.1 ]➖➖➖➖➖➖➖')
 print(' ')
 for n in range(number):
-    password =''
-    for i in range(length):
-        password += random.choice(chars)
-    print('https://t.me/BTC_CHANGE_BOT?start=b_',password)
-
+    password =''
+    for i in range(length):
+        password += random.choice(chars)
+    print('https://t.me/BTC_CHANGE_BOT?start=b_',password)
+print(' ')
+print('[BTCCheckGen] ℹ Все новости и обновления скрипта в нашем телеграм канале, подпишись и не пропускай новые плюшки.')
+print(' ')
+print('➖➖➖➖➖➖➖[ BTCCheckGen 1.1 ]➖➖➖➖➖➖➖')
+print(' ')
 
 elif inp == '2':
 
+chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 number = input('[BTCCheckGen] ➕ Укажите желаемое количество ссылок для генерации:'+ "\n")
 print(' ')
+length = input('[BTCCheckGen] ➕ Укажите длинну чека (советуем ввести 32):'+ "\n")
+print(' ')
 number = int(number)
-length = '32'
+length = int(length)
 print(' ')
 print('➖➖➖➖➖➖➖[ BTCCheckGen 1.1 ]➖➖➖➖➖➖➖')
 print(' ')
 print('[BTCCheckGen] ✅ Работа скрипта успешно завершена: ', number, ' чеков сгенерировано.')
+print('[BTCCheckGen] ⚠️ ВНИМАНИЕ: Скрипт генерирует рандомные чеки, проверять на валидность придется вручную.')
+print(' ')
 print('➖➖➖➖➖➖➖[ BTCCheckGen 1.1 ]➖➖➖➖➖➖➖')
 print(' ')
 for n in range(number):
-    password =''
-    for i in range(length):
-        password += random.choice(chars)
-    print('https://t.me/BTC_CHANGE_BOT?start=b_',password)
+    password =''
+    for i in range(length):
+        password += random.choice(chars)
+    print('https://t.me/BTC_CHANGE_BOT?start=b_',password)
+print(' ')
+print('[BTCCheckGen] ℹ Все новости и обновления скрипта в нашем телеграм канале, подпишись и не пропускай новые плюшки.')
+print(' ')
+print('➖➖➖➖➖➖➖[ BTCCheckGen 1.1 ]➖➖➖➖➖➖➖')
+print(' ')
+
+
 else:
     print("[ProxyGo] ⛔ Произошла ошибка! Повторите попытку.")
 toexit = input("[ProxyGo] ⚠️ Нажмите любую клавишу для завершения.")
